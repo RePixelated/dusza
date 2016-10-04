31e97946e941b6fade10a4e76cf2b5effb0295cd
Zalán Bálint Lévai <thatrepixelated@gmail.com>
2016-10-05 01:21:21 +0200

    Added data structure: Nevezés, Diák

---
+++ 2015_CompetitionScoring/2015_CompetitionScoring/data.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2015_CompetitionScoring
+{
+    class Nevezés
+    {
+        int verseny;
+        string név;
+        int nevIdő;
+        string iskola;
+        string megye;
+        Diák[] diákok;
+        string csapat;
+
+        public Nevezés()
+        {
+
+        }
+    }
+
+    class Diák
+    {
+        string név;
+        int évfolyam;
+        string tanár;
+    }
+}
