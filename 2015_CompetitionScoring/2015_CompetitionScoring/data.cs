d1bf154c07c0a784de329d5f068871e318f62264
RePixelated <thatrepixelated@gmail.com>
2016-10-10 15:36:55 +0200

    Added "Nevezés" processing

--- 2015_CompetitionScoring/2015_CompetitionScoring/data.cs
+++ 2015_CompetitionScoring/2015_CompetitionScoring/data.cs
@@ -3,29 +3,79 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace _2015_CompetitionScoring
 {
-    class Nevezés
+    class Nevezések
     {
-        int verseny;
-        string név;
-        int nevIdő;
-        string iskola;
-        string megye;
-        Diák[] diákok;
-        string csapat;
+        List<Nevezés> nevezések = new List<Nevezés>();
 
-        public Nevezés()
+        public Nevezések()
         {
+            StreamReader forrás = new StreamReader("nevezes.txt");
 
+            int i = 0;
+            while (i < File.ReadLines("nevezes.txt").Count())
+            {
+                nevezések.Add(new Nevezés(forrás.ReadLine()));
+                i++;
+            }
         }
     }
 
-    class Diák
+    class Nevezés
     {
-        string név;
+        int verseny;
+        string csapat;
+        int nevIdő;
+        int hónap;
+        int nap;
+        string iskola;
+        string megye;
+        string diák;
+        List<string> diákok = new List<string>();
         int évfolyam;
         string tanár;
+
+        public Nevezés(string forrás)
+        {
+            string[] narancs = forrás.Split(';');
+
+            /// Verseny kódja
+            verseny = Convert.ToInt32(narancs[0]);
+
+            /// Csapat neve
+            csapat = narancs[1];
+
+            /// Nevezés ideje
+            hónap = Convert.ToInt32(narancs[2].Substring(0, 2));
+            nap = Convert.ToInt32(narancs[2].Substring(2, 2));
+
+            nevIdő = 0;
+            for (int i = 1; i <= hónap; i++)
+                nevIdő += i % 2 == 0 ? (i == 2 ? 28 : 30) : 31;
+            nevIdő += nap;
+
+            /// Iskola neve
+            iskola = narancs[3];
+
+            /// Megye neve
+            megye = narancs[4];
+
+            /// Diákok neve
+            if (verseny == 1)
+                for (int i = 0; i < 3; i++)
+                    diákok.Add(narancs[5 + i]);
+
+            else
+                diák = narancs[5];
+
+            /// Évfolyam
+            évfolyam = Convert.ToInt32(verseny == 1 ? narancs[8] : narancs[6]);
+
+            /// Felkészítő tanár
+            tanár = verseny == 1 ? narancs[9] : narancs[7];
+        }
     }
 }
